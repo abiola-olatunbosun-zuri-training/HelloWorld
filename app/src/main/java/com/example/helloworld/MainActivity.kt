a41796799c82f9b38f9969355ad352e4eff4063b
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2021-04-27 12:33:11 +0100

    Added code comments in MainActivity.kt

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -10,14 +10,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+// Assigning buttons and text view resources to immutable variables
         val subtraction = findViewById<Button>(R.id.minus_button)
         val text = findViewById<TextView>(R.id.text_view)
         val addition = findViewById<Button>(R.id.plus_button)
+
+// Declaring variable count, and initializing with value of 0
         var count = 0
 
+ // Setting the onClick listener function for subtraction button
         subtraction.setOnClickListener {
+            // Function for decrementing by 1 at each button click
             fun onClick(){
                 count-=1
+ // Displaying operation result in text view, and converting result from type Int to String
                 text.text = count.toString()
             }
 
@@ -25,9 +31,12 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+// Setting the onClick listener function for addition button
         addition.setOnClickListener {
+            // Function for incrementing by 1 at each button click
             fun onClick(){
                 count+=1
+// Displaying operation result in text view, and converting result from type Int to String
                 text.text = count.toString()
             }
 
